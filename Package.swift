3e85244a959f687275c2a4a4d895aebcacc6feba
Calvin <me@iCalvin.dev>
2024-01-20 19:22:27 -0500

    Add missing platforms

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [
         .iOS("17.0"),
         .macOS("14.0"),
+        .watchOS("10.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
